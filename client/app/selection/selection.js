37ea6385d05368f018aaedf08c1659ff0351c49e
Boya Jiang <jiang.boya@yahoo.com>
2016-03-13 15:17:57 -0700

    Implement basic front-end for selection

--- client/app/selection/selection.js
+++ client/app/selection/selection.js
@@ -1,3 +1,13 @@
 angular.module('babel.selection', [])
-.controller('SelectionController', function ($scope, $window) {
+.controller('SelectionController', function ($location, $scope, $window, Language) {
+
+	$scope.test="TEST";
+	while (!$scope.languageChoices) {
+		$scope.languageChoices = Language.choices;
+		$scope.selectionEntered = function () {
+		Language.set($scope.lang1, $scope.lang2);
+		$location.path("/translator");
+		};
+	};
+
 });
