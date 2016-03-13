96d814b4c997e05dea640487642806350093748a
Boya Jiang <jiang.boya@yahoo.com>
2016-03-13 15:18:30 -0700

    Create skeleton for translator model/view

--- client/app/translator/translator.js
+++ client/app/translator/translator.js
@@ -1,4 +1,6 @@
 angular.module('babel.translator', [])
-.controller('TranslatorController', function ($scope, $window) {
+.controller('TranslatorController', function ($scope, $window, Language) {
+
+console.log(Language.lang1, Language.lang2);
     
 });
